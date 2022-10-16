5dd9d3f40d094d43a3d9595c3abe5b6da24b20d5
Praneetha Rao <praneetharao37@gmail.com>
2022-10-16 14:24:31 +0530

    commit code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Light from './Light';
 import './App.css';
 
 function App() {
 
   const [trafficColor, setColor] = useState('');
+  const [intervalId, setIntervalId] = useState(0);
 
   const colors= ['red', 'yellow', 'green'];
   let i = 0;
 
-  var timeout;
-  const myInterval = () => {
-     timeout = setInterval(() =>{
+
+
+  const startLightChange = () => {
+    setColor('red');
+    i=i+1;
+     const timeout = setInterval(() =>{
       setColor(colors[i]);
       i=(i+1)% colors.length;
     },5000) 
+    setIntervalId(timeout);
   }
 
   const stopLightChange = () => {
-    clearInterval(timeout);
+    clearInterval(intervalId);
+    setIntervalId(0);
     setColor('white');
-
+    return
   }
 
 
@@ -31,8 +37,8 @@ function App() {
           <Light  color ={color} trafficColor={trafficColor}/>
         )
       })}
-      <button clasName= 'startButton'onClick={myInterval}>Start</button>
-      <button  className= 'stopButton'onClick={stopLightChange}>Stop</button>
+      <button className='startButton' onClick={startLightChange}>Start</button>
+      <button  className='stopButton' onClick={stopLightChange}>Stop</button>
     
     </div>
   );
